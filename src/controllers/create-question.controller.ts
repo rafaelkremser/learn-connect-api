5a9569ca90387f3e89742463797b9786b156edf4
Rafael Kremser <rafaelfreitaskremser@gmail.com>
2024-07-21 02:26:55 -0300

    feat: authentication decoration for get user infos

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,18 +1,11 @@
 import { UseGuards, UsePipes } from '@nestjs/common';
 import { Body, Controller, HttpCode, Post } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { CurrentUser } from 'src/auth/current-user-decorator';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { UserPayload } from 'src/auth/jwt.strategy';
 import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { z } from 'zod';
-
-const createQuestionBodySchema = z.object({
-    title: z.string(),
-    content: z.string().email(),
-    password: z.string(),
-});
-
-type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>;
 
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
@@ -21,6 +14,7 @@ export class CreateQuestionController {
 
     @Post()
     @HttpCode(201)
-    @UsePipes(new ZodValidationPipe(createQuestionBodySchema))
-    async handle(@Body() body: CreateQuestionBodySchema) {}
+    async handle(@CurrentUser() user: UserPayload) {
+        console.log(user.sub);
+    }
 }
