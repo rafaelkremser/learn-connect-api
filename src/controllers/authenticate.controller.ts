0ca73cf5f7891f7b19eeefb31fe4562744684bca
Rafael Kremser <rafaelfreitaskremser@gmail.com>
2024-07-21 01:13:02 -0300

    feat: finish authenticate controller

--- src/controllers/authenticate.controller.ts
+++ src/controllers/authenticate.controller.ts
@@ -1,13 +1,18 @@
-import { ConflictException, UsePipes } from '@nestjs/common';
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    HttpCode,
+    Post,
+    UnauthorizedException,
+    UsePipes,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { hash } from 'bcryptjs';
+import { compare } from 'bcryptjs';
 import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { z } from 'zod';
 
 const authenticateBodySchema = z.object({
-    name: z.string(),
     email: z.string().email(),
     password: z.string(),
 });
@@ -16,14 +21,28 @@ type AuthenticateBodySchema = z.infer<typeof authenticateBodySchema>;
 
 @Controller('/sessions')
 export class AuthenticateController {
-    constructor(private jwt: JwtService) {}
+    constructor(private jwt: JwtService, private prisma: PrismaService) {}
 
     @Post()
-    // @HttpCode(201)
-    // @UsePipes(new ZodValidationPipe(authenticateBodySchema))
+    @HttpCode(200)
+    @UsePipes(new ZodValidationPipe(authenticateBodySchema))
     async handle(@Body() body: AuthenticateBodySchema) {
-        const token = this.jwt.sign({ sub: 'user-id' });
+        const { email, password } = body;
 
-        return token;
+        const user = await this.prisma.user.findUnique({ where: { email } });
+
+        if (!user) {
+            throw new UnauthorizedException('Invalid credentials.');
+        }
+
+        const isPasswordValid = await compare(password, user.password);
+
+        if (!isPasswordValid) {
+            throw new UnauthorizedException('Invalid credentials.');
+        }
+
+        const accessToken = this.jwt.sign({ sub: user.id });
+
+        return { access_token: accessToken };
     }
 }
