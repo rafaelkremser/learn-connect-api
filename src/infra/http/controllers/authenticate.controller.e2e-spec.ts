0636610e81cd3e17a4f8b2020d2f10ca1eb4758f
Rafael Kremser <rafaelfreitaskremser@gmail.com>
2024-07-27 23:43:11 -0300

    refactor: refactor create account controller

--- src/infra/http/controllers/authenticate.controller.e2e-spec.ts
+++ src/infra/http/controllers/authenticate.controller.e2e-spec.ts
@@ -37,7 +37,7 @@ describe('Authenticate (E2E)', () => {
 
     expect(response.statusCode).toBe(200)
     expect(response.body).toEqual({
-      access_token: expect.any(String),
+      accessToken: expect.any(String),
     })
   })
 })
