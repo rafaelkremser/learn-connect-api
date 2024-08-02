8318e83f3a03f44a8cda72f7e303aa1329614665
Rafael Kremser <rafaelfreitaskremser@gmail.com>
2024-08-01 23:07:12 -0300

    feat: create question and student factories for e2e tests

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -5,10 +5,13 @@ import {
   Question,
   QuestionProps,
 } from '@/domain/forum/enterprise/entities/question'
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { PrismaQuestionMapper } from '@/infra/database/prisma/mappers/prisma-question-mapper'
 
 export function makeQuestion(
   override: Partial<QuestionProps> = {},
-  id?: UniqueEntityID,
+  id?: UniqueEntityID
 ) {
   const question = Question.create(
     {
@@ -17,8 +20,25 @@ export function makeQuestion(
       content: faker.lorem.text(),
       ...override,
     },
-    id,
+    id
   )
 
   return question
+}
+
+@Injectable()
+export class QuestionFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaQuestion(
+    data: Partial<QuestionProps> = {}
+  ): Promise<Question> {
+    const question = makeQuestion(data)
+
+    await this.prisma.question.create({
+      data: PrismaQuestionMapper.toPrisma(question),
+    })
+
+    return question
+  }
 }
