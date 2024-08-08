e7c3135efd6c20aceaa53b287def73df68028807
Rafael Kremser <rafaelfreitaskremser@gmail.com>
2024-08-08 00:08:11 -0300

    feat: fetch question comments controller

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -33,6 +33,8 @@ import { CommentOnAnswerController } from './controllers/comment-on-answer.contr
 import { CommentOnAnswerUseCase } from '@/domain/forum/application/use-cases/comment-on-answer'
 import { DeleteAnswerCommentController } from './controllers/delete-answer-comment.controller'
 import { DeleteAnswerCommentUseCase } from '@/domain/forum/application/use-cases/delete-answer-comment'
+import { FetchQuestionCommentsController } from './controllers/fetch-question-comments.controller'
+import { FetchQuestionCommentsUseCase } from '@/domain/forum/application/use-cases/fetch-question-comments'
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
@@ -51,6 +53,7 @@ import { DeleteAnswerCommentUseCase } from '@/domain/forum/application/use-cases
     EditAnswerController,
     EditQuestionController,
     FetchQuestionAnswersController,
+    FetchQuestionCommentsController,
     FetchRecentQuestionsController,
     GetQuestionBySlugController,
   ],
@@ -69,6 +72,7 @@ import { DeleteAnswerCommentUseCase } from '@/domain/forum/application/use-cases
     EditAnswerUseCase,
     EditQuestionUseCase,
     FetchQuestionAnswersUseCase,
+    FetchQuestionCommentsUseCase,
     FetchRecentQuestionsUseCase,
     GetQuestionBySlugUseCase,
   ],
