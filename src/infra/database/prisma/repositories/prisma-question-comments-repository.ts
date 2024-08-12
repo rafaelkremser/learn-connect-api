903f8f5361c41b2b26b7abca5864f817585595f0
Rafael Kremser <rafaelfreitaskremser@gmail.com>
2024-08-12 20:36:18 -0300

    refactor: fetch question comments with author controller

--- src/infra/database/prisma/repositories/prisma-question-comments-repository.ts
+++ src/infra/database/prisma/repositories/prisma-question-comments-repository.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@nestjs/common'
 import { PrismaService } from '../prisma.service'
 import { PrismaQuestionCommentMapper } from '../mappers/prisma-question-comment-mapper'
 import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment'
+import { PrismaCommentWithAuthorMapper } from '../mappers/prisma-comment-with-author-mapper'
 
 @Injectable()
 export class PrismaQuestionCommentsRepository
@@ -32,6 +33,23 @@ export class PrismaQuestionCommentsRepository
     })
 
     return questionComments.map(PrismaQuestionCommentMapper.toDomain)
+  }
+
+  async findManyByQuestionIdWithAuthor(
+    questionId: string,
+    { page }: PaginationParams
+  ) {
+    const questionComments = await this.prisma.comment.findMany({
+      where: { questionId },
+      include: {
+        author: true,
+      },
+      orderBy: { createdAt: 'desc' },
+      take: 20,
+      skip: (page - 1) * 20,
+    })
+
+    return questionComments.map(PrismaCommentWithAuthorMapper.toDomain)
   }
 
   async create(questionComment: QuestionComment) {
