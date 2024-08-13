eed7874eecf59eae077f3fc18f34fdfd3f5d8a7c
Rafael Kremser <rafaelfreitaskremser@gmail.com>
2024-08-12 21:20:41 -0300

    refactor: fetch answer comments response

--- src/domain/forum/application/use-cases/fetch-answer-comments.spec.ts
+++ src/domain/forum/application/use-cases/fetch-answer-comments.spec.ts
@@ -2,47 +2,85 @@ import { FetchAnswerCommentsUseCase } from './fetch-answer-comments'
 import { InMemoryAnswerCommentsRepository } from 'test/repositories/in-memory-answer-comments-repository'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { makeAnswerComment } from 'test/factories/make-answer-comment'
+import { makeStudent } from 'test/factories/make-student'
+import { InMemoryStudentsRepository } from 'test/repositories/in-memory-students-repository'
 
+let inMemoryStudentsRepository: InMemoryStudentsRepository
 let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository
 let sut: FetchAnswerCommentsUseCase
 
 describe('Fetch Answer Comments', () => {
   beforeEach(() => {
-    inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository()
+    inMemoryStudentsRepository = new InMemoryStudentsRepository()
+    inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository(
+      inMemoryStudentsRepository
+    )
     sut = new FetchAnswerCommentsUseCase(inMemoryAnswerCommentsRepository)
   })
 
   it('should be able to fetch answer comments', async () => {
-    await inMemoryAnswerCommentsRepository.create(
-      makeAnswerComment({
-        answerId: new UniqueEntityID('answer-01'),
-      }),
-    )
-    await inMemoryAnswerCommentsRepository.create(
-      makeAnswerComment({
-        answerId: new UniqueEntityID('answer-01'),
-      }),
-    )
-    await inMemoryAnswerCommentsRepository.create(
-      makeAnswerComment({
-        answerId: new UniqueEntityID('answer-01'),
-      }),
-    )
+    const student = makeStudent({
+      name: 'John Doe',
+    })
+
+    await inMemoryStudentsRepository.create(student)
+
+    const comment1 = makeAnswerComment({
+      authorId: student.id,
+      answerId: new UniqueEntityID('answer-01'),
+    })
+    const comment2 = makeAnswerComment({
+      authorId: student.id,
+      answerId: new UniqueEntityID('answer-01'),
+    })
+    const comment3 = makeAnswerComment({
+      authorId: student.id,
+      answerId: new UniqueEntityID('answer-01'),
+    })
+
+    await inMemoryAnswerCommentsRepository.create(comment1)
+
+    await inMemoryAnswerCommentsRepository.create(comment2)
+
+    await inMemoryAnswerCommentsRepository.create(comment3)
 
     const result = await sut.handle({
       answerId: 'answer-01',
       page: 1,
     })
 
-    expect(result.value?.answerComments).toHaveLength(3)
+    expect(result.value?.comments).toHaveLength(3)
+    expect(result.value?.comments).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          author: 'John Doe',
+          commentId: comment1.id,
+        }),
+        expect.objectContaining({
+          author: 'John Doe',
+          commentId: comment2.id,
+        }),
+        expect.objectContaining({
+          author: 'John Doe',
+          commentId: comment3.id,
+        }),
+      ])
+    )
   })
 
   it('should be able to fetch paginated recent answers', async () => {
+    const student = makeStudent({
+      name: 'John Doe',
+    })
+
+    await inMemoryStudentsRepository.create(student)
+
     for (let i = 1; i <= 22; i++) {
       await inMemoryAnswerCommentsRepository.create(
         makeAnswerComment({
           answerId: new UniqueEntityID('answer-01'),
-        }),
+          authorId: new UniqueEntityID(student.id.toString()),
+        })
       )
     }
 
@@ -51,6 +89,6 @@ describe('Fetch Answer Comments', () => {
       page: 2,
     })
 
-    expect(result.value?.answerComments).toHaveLength(2)
+    expect(result.value?.comments).toHaveLength(2)
   })
 })
--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -1,11 +1,15 @@
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
 import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
+import { InMemoryStudentsRepository } from './in-memory-students-repository'
+import { CommentWithAuthor } from '@/domain/forum/enterprise/entities/value-objects/comment-with-author'
 
 export class InMemoryAnswerCommentsRepository
   implements AnswerCommentsRepository
 {
   public items: AnswerComment[] = []
+
+  constructor(private studentsRepository: InMemoryStudentsRepository) {}
 
   async findById(id: string) {
     const answerComment = this.items.find((item) => item.id.toString() === id)
@@ -25,13 +29,44 @@ export class InMemoryAnswerCommentsRepository
     return answerComments
   }
 
+  async findManyByAnswerIdWithAuthor(
+    answerId: string,
+    params: PaginationParams
+  ) {
+    const answerComments = this.items
+      .filter((item) => item.answerId.toString() === answerId)
+      .slice((params.page - 1) * 20, params.page * 20)
+      .map((comment) => {
+        const author = this.studentsRepository.items.find((student) => {
+          return student.id.equals(comment.authorId)
+        })
+
+        if (!author) {
+          throw new Error(
+            `Author with ID ${comment.authorId.toString()} does not exists.`
+          )
+        }
+
+        return CommentWithAuthor.create({
+          commentId: comment.id,
+          content: comment.content,
+          author: author.name,
+          authorId: comment.authorId,
+          createdAt: comment.createdAt,
+          updatedAt: comment.updatedAt,
+        })
+      })
+
+    return answerComments
+  }
+
   async create(answerComment: AnswerComment) {
     this.items.push(answerComment)
   }
 
   async delete(answerComment: AnswerComment) {
     const itemIndex = this.items.findIndex(
-      (item) => item.id === answerComment.id,
+      (item) => item.id === answerComment.id
     )
 
     this.items.splice(itemIndex, 1)
